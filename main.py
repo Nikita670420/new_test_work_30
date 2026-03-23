a78dedadac6ed715f665b9e4f648d53bcb63a7f0
root <root@WIN-7FBVHLP1HFV.localdomain>
2026-03-23 12:25:10 +0500

    FastAPI homework module 26 (демонстрация)

--- main.py
+++ main.py
@@ -88,3 +88,7 @@ async def create_recipe(recipe_data: RecipeCreate, session: AsyncSession = Depen
         ingredients=ingredients,
         description=recipe.description
     )
+
+
+
+# ЭТОТ КОММЕНТАРИЙ НУЖЕН, ЧТОБ БЫЛА СМЕНА КОДА (ЧТОБ МЕЖДУ ПРОВЕРКОЙ И ДЕМОНСТРАЦИЕЙ БЫЛА РАЗНИЦА)
